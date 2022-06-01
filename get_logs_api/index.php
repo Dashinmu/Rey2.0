544067be33564f3d890993ddbd2b429ae4fac6b8
Dashinmu <90204594+Dashinmu@users.noreply.github.com>
2022-06-01 10:08:45 +0500

    Changed the log output

--- get_logs_api/index.php
+++ get_logs_api/index.php
@@ -30,18 +30,13 @@
 
             $logs = []; //Массив с логами
     
-            //Переменные и проблемные ошибки
-            $flag = false;
-            $log_oAuthStackTraceError = 'League\OAuth2\Server\Exception\OAuthServerException';
-            $log_oAuthStackTraceWarning = 'PDOException:';
-            $log_arrayStack = 'array (';
-            $log_debugError  = 'Debug\Exception\FatalErrorException';
-            //$log_null = '  ';
+            //Переменные
             $date_string = '';
             $type_string = '';
             $info_string = '';
 
-            $log_file_name = $file_name; //Имя файла с логом
+            $dir = __DIR__ . '/logs/';
+            $log_file_name = $dir.$file_name; //Имя файла с логом
     
             $f = fopen($log_file_name, "r"); //Открыть файл логов
 
@@ -51,32 +46,27 @@
                 //Флаг лога, все логи начинаются с '['
                 if ($line[0] == '[') 
                 {
-                    $flag = true;
+                    if ( !empty( trim($info_string) ) )
+                    {
+                        $index = ['date' => $date_string, 'type' => $type_string, 'info' => $info_string];
+                        array_push($logs, $index);
+                    }
+
                     $date_index_end = strpos($line, ']');
                     $date_string = substr($line, 1, $date_index_end - 1);
                     $type_index_spacer = strpos($line, ':', $date_index_end);
                     $type_string = substr($line, $date_index_end + 2, $type_index_spacer - $date_index_end - 2);
                     $info_string = substr($line, $type_index_spacer + 2, strlen($line));
-                    if ( ( str_contains($info_string, $log_oAuthStackTraceError) == true ) or (str_contains($info_string, $log_oAuthStackTraceWarning) == true ) or (str_contains($info_string, $log_arrayStack) == true ) or ( ($info_string[0] == ' ') and ($info_string[1] == ' ') ) or ( str_contains($info_string, $log_debugError) == true  ) ) //Если строка информации содержит указанные подстроки - продолжать запись лога 
-                    {
-                        $flag = false;
-                    }
                 } else 
                 {
-                    $flag = false;
                     $info_string = $info_string.$line;
-                    if ( ( $line[0] == ')' ) or ( str_contains($info_string, '{main}') == true ) ) //Если строка содержит указанные подстроки - конец записи лога
-                    {
-                        $flag = true;
-                    }
                 }
-
-                //По флагу добавляем в logs, ибо не в каждой строчке лог
-                if ($flag == true) 
-                {
-                    $index = ['date' => $date_string, 'type' => $type_string, 'info' => $info_string];
-                    array_push($logs, $index);
-                }
+            }
+            //Запись последнего лога
+            if ( !empty( trim($info_string) ) )
+            {
+                $index = ['date' => $date_string, 'type' => $type_string, 'info' => $info_string];
+                array_push($logs, $index);
             }
             return($logs);
         }
